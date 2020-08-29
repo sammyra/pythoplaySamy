b9437e9f03f07b6cef7bffd98a086f061f5d71f7
juan <juan@ifitness.com.br>
2020-08-28 22:30:52 -0300

    adicao comentario explicativo

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
 
+"""
+    @brief: evalua tres numeros que recibe como parametros.
+            Suma los dos primeros y multiplica el tercero 
+            al resultado si cumple la condicion definida  
+
+    @n1: valor ingresado por el usuario   
+    @n2: valor ingresado por el usuario
+    @n3: valor a evaluar a la condicion
+"""
 
 def ejemplo_none(n1, n2, n3 = None):
     resultado = n1 + n2 
@@ -7,6 +16,6 @@ def ejemplo_none(n1, n2, n3 = None):
 
     return resultado
 
-print(ejemplo_none(4,2,0))
+print(ejemplo_none(4,4))
 
 
